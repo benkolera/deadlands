349a7d3c54eef2a306be0108ff3844b5bce597ce
Ben Kolera <ben.kolera@polymathian.com>
2018-10-03 12:09:29 +1000

    Next up, the aptitude sets.

--- common/src/Common/CharacterSheet.hs
+++ common/src/Common/CharacterSheet.hs
@@ -1,12 +1,41 @@
-{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module Common.CharacterSheet where
 
-import           Control.Lens          (from, to, (^.))
-import           Control.Lens.TH       (makeLenses)
-import           Data.Number.Nat1      (Nat1)
+import           Control.Lens     (from, to, (^.))
+import           Control.Lens.TH  (makeLenses)
+import           Data.Number.Nat1 (Nat1)
 
-data Traits = Traits deriving (Eq, Show)
+import           Common.DiceSet   (DiceSet)
+
+data DeftnessAptitudes = DeftnessAptitudes
+data NimblenessAptitudes = NimblenessAptitudes
+data QuicknessAptitudes = QuicknessAptitudes
+data StrengthAptitudes = StrengthAptitudes
+data VigorAptitudes = VigorAptitudes
+data CognitionAptitudes = CognitionAptitudes
+data KnowledgeAptitudes = KnowledgeAptitudes
+data MienAptitudes = MienAptitudes
+data SmartsAptitudes = SmartsAptitudes
+data SpiritAptitudes = SpiritAptitudes
+
+data Trait a = Trait
+  { _traitDiceSet   :: DiceSet
+  , _traitAptitudes :: a
+  }
+
+data Traits = Traits
+  { _traitsDeftness   :: Trait DeftnessAptitudes
+  , _traitsNimbleness :: Trait NimblenessAptitudes
+  , _traitsQuickness  :: Trait QuicknessAptitudes
+  , _traitsStrength   :: Trait StrengthAptitudes
+  , _traitsVigor      :: Trait VigorAptitudes
+  , _traitsCognition  :: Trait CognitionAptitudes
+  , _traitsKnowledge  :: Trait KnowledgeAptitudes
+  , _traitsMien       :: Trait MienAptitudes
+  , _traitsSmarts     :: Trait SmartsAptitudes
+  , _traitsSpirit     :: Trait SpiritAptitudes
+  } deriving (Eq, Show)
 
 data EffectSet = EffectSet deriving (Eq, Show)
 

ecf52e78209abd68f8e440b54511021255cea4ac
Ben Kolera <ben.kolera@gmail.com>
2019-01-17 16:58:02 +1000

    WIP

--- default.nix
+++ default.nix
@@ -1,10 +1,14 @@
 { system ? builtins.currentSystem # TODO: Get rid of this system cruft
 , iosSdkVersion ? "10.2"
+, hie ? false
 }:
-with import ./.obelisk/impl { inherit system iosSdkVersion; };
+let 
+  hie-nix = (import ./nix/hie-nix.nix { ghc843 = true; inherit hie; });
+in with import ./.obelisk/impl { inherit system iosSdkVersion; };
 project ./. ({ ... }: {
   android.applicationId = "systems.obsidian.obelisk.examples.minimal";
   android.displayName = "Obelisk Minimal Example";
   ios.bundleIdentifier = "systems.obsidian.obelisk.examples.minimal";
   ios.bundleName = "Obelisk Minimal Example";
-})
+  withHoogle = true;
+  tools = _: hie-nix.hie-tools;
--- frontend/.dir-locals.el
+++
@@ -1,4 +0,0 @@
-((nil . (
-  (dante-target . "test:tests")
-  (dante-repl-command-line . ("nix-shell" "--run" (concat "cabal new-repl " dante-target " --builddir=dist/dante")))
-)))

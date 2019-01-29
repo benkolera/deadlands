9c16803d716770c69fe4552edca5949728d42554
Ben Kolera <ben.kolera@gmail.com>
2019-01-29 12:51:52 +1000

    This works. Need the endo over every leaf now.

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -11,6 +11,7 @@ import           Control.Lens
 
 import           Clay                     (render)
 import qualified Data.Map                 as Map
+import           Data.Monoid.Endo         (Endo(Endo), runEndo)
 import           Data.Number.Nat          (Nat, fromNat)
 import qualified Data.Text.Lazy           as TL
 import           Data.Text.Lens           (packed)
@@ -21,7 +22,7 @@ import           Reflex.Dom
 import           Common.CharacterSheet
 import           Common.DiceSet
 import           Common.Route
-import           Frontend.Internal        (fget)
+import           Frontend.Internal        (fget, diffDyn)
 import           Frontend.Style
 import           Frontend.Traits          (traits)
 import           Frontend.Wounds          (Limbs (Limbs), wounds)
@@ -41,8 +42,12 @@ info bgDyn = elClass "div" "info" $ do
     el "dt" $ text "Grit"
     el "dd" $ dynText (fget (chrBgGrit . to show . packed) bgDyn)
 
-applyWounds :: CharacterSheet DiceSet -> Nat -> CharacterSheet DiceSet
-applyWounds c wl = c & chrSheetTraits.traitsDeftness.traitAptitudes.deftnessShootin.concentrationLevel.diceSetBonus %~ (\x -> x - fromNat wl)
+
+woundEffects :: Reflex t => Dynamic t Nat -> Event t (Endo (CharacterSheet DiceSet))
+woundEffects = diffDyn $ \old new -> Endo $ \c -> applyWounds c (fromNat new - fromNat old)
+
+applyWounds :: CharacterSheet DiceSet -> Integer -> CharacterSheet DiceSet
+applyWounds c wl = c & chrSheetTraits.traitsDeftness.traitAptitudes.deftnessShootin.concentrationLevel.diceSetBonus %~ (\x -> x - wl)
 
 frontend :: Frontend (R FrontendRoute)
 frontend = Frontend
@@ -51,27 +56,21 @@ frontend = Frontend
       el "style" . text . TL.toStrict . render $ style
   , _frontend_body = prerender (text "Loading...") $ elClass "div" "app" $ mdo
       let chrDs  = calculateDiceSets gabriela
-      -- This forms an infinite recursion because we can't make maxWounds until we have a chrDyn
-      -- fix this by making a function woundChanges :: Dynamic t Nat -> Event t (Endo CharacterSheet)
-      let chrDyn = applyWounds chrDs <$> maxWoundsE
-      --let chrDyn = constDyn (calculateDiceSets gabriela)
-      maxWoundsE <- elClass "div" "character-sheet" $ do
+      chrDyn <- foldDyn (flip runEndo) chrDs woundsDiffE
+      woundsDiffE <- elClass "div" "character-sheet" $ do
         elClass "div" "traits" $ do
           traits (fget chrSheetTraits chrDyn)
           blank
-        maxWoundsE <- elClass "div" "effects" $ do
+        maxWoundsDyn <- elClass "div" "effects" $ do
           info (fget chrSheetBackground chrDyn)
-          sizeDyn    <- holdUniqDyn (fget chrSheetSize chrDyn)
-          vigorSides <- holdUniqDyn (fget (chrSheetTraits.traitsVigor.traitDiceSet.diceSetSides.to sidesToNat.to (*2)) chrDyn)
-          lightArmor <- holdUniqDyn (fget chrSheetLightArmor chrDyn)
-          wounds (traceDyn "size" sizeDyn) (traceDyn "vigor" vigorSides) (traceDyn "armor" lightArmor)
-             (Limbs 0 0 (Just 0) (Just 0) (Just 0) (Just 0))
+          wounds
+            (fget chrSheetSize chrDyn)
+            (fget (chrSheetTraits.traitsVigor.traitDiceSet.diceSetSides.to sidesToNat.to (*2)) chrDyn)
+            (fget chrSheetLightArmor chrDyn)
+            (Limbs 0 0 (Just 0) (Just 0) (Just 0) (Just 0))
         elClass "div" "spells" $ do
           el "h1" $ text "Blessings"
           blank
-        pure (traceDyn "max wounds" maxWoundsE)
+        pure (woundEffects maxWoundsDyn)
       pure ()
-      --display chrDynBad
-      display maxWoundsE
-
   }
--- frontend/src/Frontend/Internal.hs
+++ frontend/src/Frontend/Internal.hs
@@ -2,6 +2,10 @@
 module Frontend.Internal where
 
 import Control.Lens (Getter, view)
+import Reflex (Reflex, Dynamic, Event, attach, current, updated)
 
 fget :: Functor f => Getter a b -> f a -> f b
 fget g = fmap (view g)
+
+diffDyn :: Reflex t => (a -> a -> b) -> Dynamic t a -> Event t b
+diffDyn f d = uncurry f <$> attach (current d) (updated d)
